1e15cd9d0f8676b4a4e630c7743bc7b426d0599c
CYBAI <cyb.ai.815@gmail.com>
2017-12-02 01:10:14 +0800

    Use `chars()` to transform string into vector

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -4,25 +4,25 @@
 
 pub fn parse_sequence(input: &str) -> i32 {
     let mut total: i32 = 0;
-    let chars: Vec<&str> = input.split("").collect();
-    let len = chars.len() - 3;
-    let last_empty = chars.len() - 1;
-    let new_line_idx = chars.len() - 2;
+    let chars: Vec<char> = input.chars().collect();
 
-    for (idx, each_char) in input.split("").into_iter().enumerate() {
-        if idx == 0 || idx == last_empty || idx == new_line_idx {
+    let len = chars.len() - 2;
+    let new_line_idx = chars.len() - 1;
+
+    for (idx, each_char) in input.chars().enumerate() {
+        if idx == new_line_idx {
             continue;
         }
 
         let check_idx: usize = if idx == len {
-            1
+            0
         } else {
             idx + 1
         };
 
         if each_char == chars[check_idx] {
-            if let Ok(int) = each_char.parse::<i32>() {
-                total += int;
+            if let Some(int) = each_char.to_digit(10) {
+                total += int as i32;
             }
         }
     }
