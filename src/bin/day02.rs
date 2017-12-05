1a4f82755c2c9bb4465bc8d5d12f324d20d2b3f8
CYBAI <cyb.ai.815@gmail.com>
2017-12-06 00:14:36 +0800

    Implement day 2 part 2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,11 +1,12 @@
 extern crate aoc17;
 
-use aoc17::day02::checksum;
+use aoc17::day02::{checksum, evenly_divisible_values};
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
   run_part_1();
+  run_part_2();
 }
 
 fn run_part_1() {
@@ -21,4 +22,19 @@ fn run_part_1() {
   f.read_to_string(&mut contents).expect("CONTENT READ ERROR");
 
   println!("{}", checksum(contents.as_str()));
+}
+
+fn run_part_2() {
+  let input = "5 9 2 8
+9 4 7 3
+3 8 6 5";
+
+  assert_eq!(9, evenly_divisible_values(input));
+
+  let mut f = File::open("./src/day02/input-part-2").expect("FILE NOT FOUND");
+
+  let mut contents = String::new();
+  f.read_to_string(&mut contents).expect("CONTENT READ ERROR");
+
+  println!("{}", evenly_divisible_values(contents.as_str()));
 }
--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -2,6 +2,9 @@
 //! Implementation of Day 2 in Advent of Code 2017
 //!
 
+use std::cmp;
+use std::collections::{HashMap, HashSet};
+
 pub fn checksum(input: &str) -> i32 {
   input.lines()
     .fold(0, |acc, line| {
@@ -21,4 +24,65 @@ fn find_min(line: &str) -> i32 {
       .map(|n| n.parse::<i32>().unwrap())
       .min()
       .unwrap()
+}
+
+pub fn evenly_divisible_values(input: &str) -> i32 {
+  let vectors: Vec<Vec<i32>> = parse_input(input);
+
+  vectors.into_iter()
+    .fold(0, |acc, v| {
+      acc + find_evenly_divisible(v)
+    })
+}
+
+fn find_evenly_divisible(vec: Vec<i32>) -> i32 {
+  let mut map: HashMap<i32, HashSet<i32>> = HashMap::new();
+  let mut result = 0;
+
+  for n in vec {
+    let f = factors(n);
+
+    for (k, v) in map.iter() {
+      if f.intersection(&v).count() >= cmp::min(v.len(), f.len()) {
+        result = if k.clone() > n {
+          k / n
+        } else {
+          n / k
+        };
+
+        break;
+      }
+    }
+
+    if result > 0 {
+      break;
+    }
+
+    map.insert(n, f.clone());
+  }
+
+  result
+}
+
+fn factors(num: i32) -> HashSet<i32> {
+  let mut v: HashSet<i32> = HashSet::new();
+
+  let mid = (num / 2 as i32) + 1;
+
+  for i in 1..mid {
+    if num % i == 0 {
+      v.insert(num / i);
+      v.insert(i);
+    }
+  }
+
+  v
+}
+
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
+  input.lines().map(|line| {
+    line.split_whitespace()
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect()
+  }).collect()
 }
